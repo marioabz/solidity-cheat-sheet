de2fcf673ebad952a89c6e484c4d9cfdf7acaaba
mario <mario.briseno7@gmail.com>
2021-03-23 23:59:54 -0600

    added coments on data types

--- contracts/coin.sol
+++ contracts/coin.sol
@@ -3,9 +3,15 @@ pragma solidity ^0.4.21;
 
 contract Coin {
 
+    // address type holds 20 bytes values (size of Ethereum addresses)
     address public minter;
+
+    // mapping data type allows to create relationships between a 
+    // key and a value.
+    // 'mapping' works like a hash table.
     mapping (address => uint) public balances;
 
+    // Events are convenience interfaces with the EVM logging facilities.
     event Sent(address from, address to, uint amount);
 
     function Coin() public {
