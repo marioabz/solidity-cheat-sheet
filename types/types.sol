9d491c0930b8036fbd48c8253fdec443ac1c0754
mario <mario.briseno7@gmail.com>
2021-03-27 23:59:02 -0600

    added addres data type with comments

--- types/types.sol
+++ types/types.sol
@@ -19,6 +19,21 @@ contract Types {
     bool public isMarketClosed;
     bytes public gates;
 
+    // The address types comes in 2 flavors: address, and address payable.
+    // 'address payable' has 'transfer' and 'send' as aditional members.
+
+    // Explicit conversions to and from address are allowed for uint180,
+    // integer literals, bytes20 and contract types.
+
+    // Only expressions of type 'address' and contract-type can be converted
+    // to the type 'address payable' via the explicit conversion 'payable(...)
+    address public owner;
+   
+    // Implicit conversions from 'address payable' to 'address' are allowed,
+    // whereas conversions from 'address' to 'address payable' must be
+    // explicit via 'payable(<address>)'
+    address payable public backupOwner
+
     // uint and int are aliases for uint256 and int256, respectively.
     // For an integer tpye 'x', you can use tpye(X).min and type(X).max
     // to access minimum and maximum value representable by the type.
@@ -28,10 +43,13 @@ contract Types {
     constructor(
         bool _availability,
         uint _hardCap,
+        address owner,
     ) {
         hardCap = _hardCap;
         available = _availability;
         isMarketClosed = !_availability;
         gates = 1;
+        owner = msg.sender;
+
     }
 }
