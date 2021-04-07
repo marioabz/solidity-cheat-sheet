a4f1444eddcfad1b986fdc46da2dc946f8e512f0
mario <mario.briseno7@gmail.com>
2021-04-06 21:57:55 -0500

    corrected a mistake on emittion of an event

--- contracts/coin.sol
+++ contracts/coin.sol
@@ -27,6 +27,6 @@ contract Coin {
         if (balances[msg.sender] < amount) return;
         balances[msg.sender] -= amount;
         balances[receiver] += amount;
-        emit Sent(msg.sender, receiver, amount)
+        emit Sent(msg.sender, receiver, amount);
     }
 }
