1d15984b8f16c6445a4fd857b58edcd9a8e697d9
mario <mario.briseno7@gmail.com>
2021-04-13 17:14:23 -0500

    completed 'StringEqualityComparison' pattern

--- contracts/patterns/StringEqualityComparison.sol
+++ contracts/patterns/StringEqualityComparison.sol
@@ -5,4 +5,20 @@ pragma solidity ^0.8.0;
 // minimizes average gas consumption for a large number of
 // different inputs.
 contract StringEqualityComparison {
+
+    // Use the 'String Equality Comparison' pattern when:
+    // - want to check two strings for equality,
+    // - most of your strings to compare are longer than 2 characters,
+    // - want to minimize the average amount of gas needed for a broad
+    // variety of strings.
+
+    function hashCompareWithLengthCheck(
+        string memory a, 
+        string memory b) 
+        internal returns (bool) {
+        if (bytes(a).length != bytes(b).length) {
+            return false;
+        }
+        return keccak256(a) == keccak256(b);
+    }
 }
