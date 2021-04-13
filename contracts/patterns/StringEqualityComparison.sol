c40c5d717625986a8262ccb0be9de612d02d894f
mario <mario.briseno7@gmail.com>
2021-04-13 17:13:46 -0500

    added skeleton for 'StringEqualityComparison' pattern

---
+++ contracts/patterns/StringEqualityComparison.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.0;
+
+
+// Check for the equality of two provided strings in a way that 
+// minimizes average gas consumption for a large number of
+// different inputs.
+contract StringEqualityComparison {
+}
