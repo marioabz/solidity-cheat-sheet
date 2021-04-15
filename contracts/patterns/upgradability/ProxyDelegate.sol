ed9df728fa5efb91255925201a557ba7fdde31fe
mario <mario.briseno7@gmail.com>
2021-04-14 20:13:31 -0500

    added contract to the delegate proxy pattern

--- contracts/patterns/upgradability/ProxyDelegate.sol
+++ contracts/patterns/upgradability/ProxyDelegate.sol
@@ -43,4 +43,22 @@ contract ProxyDelegate {
 // more precise control over the stack machine, with a language similar to the
 // one used by the EVM and can be used within solidity code).
 
+    address delegate;
+    address owner = msg.sender;
+
+    function upgradeDelegate(address newDelegateAddress) public {
+        require(msg.sender == owner);
+        delegate = newDelegateAddress;
+    }
+
+    fallback() external payable {
+        assembly {
+            let _target := sload(0)
+            calldatacopy(0x0, 0x0, calldatasize())
+            let result := delegatecall(gas(), _target, 0x0, calldatasize(), 0x0, 0)
+            returndatacopy(0x0, 0x0, returndatasize())
+            switch result case 0 {revert(0,0)} default {return (0, returndatasize())}
+        }
+    }
+        
 }
