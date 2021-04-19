2156f80081973f47b12953ac31f24dd86a001716
mario <mario.briseno7@gmail.com>
2021-04-18 23:59:28 -0500

    added 'added comments to the EternalStorage.sol file

--- contracts/patterns/upgradability/EternalStorage.sol
+++ contracts/patterns/upgradability/EternalStorage.sol
@@ -6,7 +6,11 @@ pragma solidity ^0.8.0;
 // the contract is deployed to the network and coexists with the old
 // version. Because the old contract is not actually updated to a
 // a new version, the accumulated storage still resides at the old
-// address
+// address.
+// The solution consists of separatings the storage from the contract
+// logic. A separate contract with the sole purpose of acting as a
+// storage to another contract
+
 contract EternalStorage {
 
 
