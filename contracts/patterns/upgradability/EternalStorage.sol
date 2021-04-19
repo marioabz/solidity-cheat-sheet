4c49d2f5177651eaf2c31c2df473b9178f41dcc8
mario <mario.briseno7@gmail.com>
2021-04-18 23:54:57 -0500

    added 'Eternal Storage' pattern

---
+++ contracts/patterns/upgradability/EternalStorage.sol
@@ -0,0 +1,11 @@
+
+pragma solidity ^0.8.0;
+
+// When upgrading a smart contract, e.g. eith the help of the 'Proxy
+// Delegate' pattern, what really happens is that a new version of
+// the contract is deployed to the network and coexists with the old
+// version
+contract EternalStorage {
+
+
+}
