2c893543caea925c5f13333bad8fd66f36d81437
mario <mario.briseno7@gmail.com>
2021-05-08 00:06:23 -0500

    started commentating access restriction contract - 2

--- contracts/patterns/security_patterns/AccessRestriction.sol
+++ contracts/patterns/security_patterns/AccessRestriction.sol
@@ -6,7 +6,11 @@ pragma solidity ^0.4.0;
 // Access restriction is achieved by declaring your state variables as
 // private. Also functions can be declared 'private', however doing so
 // would prevent everyone outside the contract scope from calling it 
-// under any circunstances.
+// under any circumstances. Simply declaring them 'public', however,
+// would open up access to every participant in the network. Most of
+// the times it is desired to allow access to functionality in case
+// certain rules are met.
+
 contract AccessRestriction {
 
 }
