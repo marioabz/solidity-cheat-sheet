8afae4a6f722170b99de332e7062f3146f98ba05
mario <mario.briseno7@gmail.com>
2021-05-08 00:31:14 -0500

    added use cases of access restriction patterns

--- contracts/patterns/security_patterns/AccessRestriction.sol
+++ contracts/patterns/security_patterns/AccessRestriction.sol
@@ -10,6 +10,15 @@ pragma solidity ^0.4.0;
 // would open up access to every participant in the network. Most of
 // the times it is desired to allow access to functionality in case
 // certain rules are met.
+// Often the access is supposed to be restricted to a defined set of
+// entities, like the administrators of a contract.
+
+// Use the Access Restriction pattern when:
+// - contract functions should only be callable under certain
+// circumstances,
+// - one wants to apply similar restrictions to several functions,
+// - one wants to increase security of his smart contract against
+// unauthorized access
 
 contract AccessRestriction {
 
