724b926026350752be65a2ef12997db07d30dfd8
mario <mario.briseno7@gmail.com>
2021-05-08 00:56:37 -0500

    completed comments on AccessRestriction pattern

--- contracts/patterns/security_patterns/AccessRestriction.sol
+++ contracts/patterns/security_patterns/AccessRestriction.sol
@@ -20,6 +20,27 @@ pragma solidity ^0.4.0;
 // - one wants to increase security of his smart contract against
 // unauthorized access
 
+// Before the Byzantium update, require() and assert() behaved
+// identically. Since then the underlying opcodes actually differ. The
+// two methods require() and revert() use 0xfd (REVERT) while assert()
+// uses 0xfe (INVALID). The big difference between the two opcodes is
+// gas return. While REVERT is refunding all of the gas that has not
+// been consumed at the time the exception is thrown, INVALID uses up
+// all gas included in the transaction.
+
+// The solidity documentation suggests that require() 'should be used
+// to ensure valid conditions, such as inputs, or contract state variales
+// or to validate return values from calls to external contracts' and
+// assert() 'should only be used to test for internal errors, and to
+// check invariants'. The revert() throws in every case. It is therefore
+// useful in complex situations, like if-else trees, where the evaluation
+// of the condition can not be conduced in one line of code and the use 
+// of 'require()' would not be fitting.
+
+// Generally 'require()' should be used towards the beginning of a
+// function for validation and should be used more often than the other
+// two.
+
 contract AccessRestriction {
 
 }
